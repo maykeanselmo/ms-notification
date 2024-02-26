a696327c8e9b8af5aed63245b0a0e79549879c1f
Mayke <mayke000000000@gmail.com>
2024-02-26 14:41:13 -0300

    Commit inicial: Criação do projeto

---
+++ msnotification/src/main/java/com/compass/challenge/msnotification/MsnotificationApplication.java
@@ -0,0 +1,13 @@
+package com.compass.challenge.msnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsnotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsnotificationApplication.class, args);
+	}
+
+}
---
+++ msnotification/src/test/java/com/compass/challenge/msnotification/MsnotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compass.challenge.msnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsnotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
